db394624aca6387865e6e1e6a812ff182933d595
Josh Jaffe <josjaf@gmail.com>
2019-09-25 22:07:14 -0400

    using multiprocessing list

--- example/org_s3_print_multi_process.py
+++ example/org_s3_print_multi_process.py
@@ -1,39 +1,56 @@
 import boto3
 import jmespath
 from newport_helpers import NPH
-import threading
+
+from multiprocessing import Process
+import multiprocessing
+manager = multiprocessing.Manager()
+
 
 NPH = NPH.NPH()
 
-def process_accounts(account, master_session, results):
-    account_results = []
-    session = NPH.Helpers.get_child_session(account, 'OrganizationAccountAccessRole', master_session)
-    s3 = session.client('s3')
-    response = s3.list_buckets()
-    buckets = jmespath.search("Buckets[].Name", response)
-    for bucket in buckets:
-        account_results.append(bucket)
+def process_accounts(account, master_session, results, **kwargs):
+    try:
+        print(account)
+        account_results = []
+        session = NPH.Helpers.get_child_session(account, 'OrganizationAccountAccessRole', master_session)
+        s3 = session.client('s3')
+        response = s3.list_buckets()
 
-    account_total = {'account_id': account, 'buckets': account_results}
-    results.append(account_total)
-    print(session)
+        buckets = jmespath.search("Buckets[].Name", response)
+
+        for bucket in buckets:
+            account_results.append(bucket)
+
+        account_total = {'account_id': account, 'buckets': account_results}
+        # print(account_total)
+        results.append(account_total)
+    except Exception as e:
+        print(e)
+
+
 
     return
 
 
 def main():
-    results = []
-    threads = []
+    global results
+    results = manager.list()
+    procs = []
+    kwargs = {}
     master_session = boto3.session.Session()
     for account in NPH.Org_Helpers.get_org_accounts(master_session):
-        t = threading.Thread(target=process_accounts, args=(account, master_session, results))
-        threads.append(t)
-        t.start()
+        proc = Process(target=process_accounts,
+                    args=(account, master_session, results),
+                    kwargs=kwargs)
+        procs.append(proc)
+        proc.start()
 
         #process_accounts(account, session, results)
-    for thread in threads:
-        thread.join()
-    print(results)
+    for proc in procs:
+        proc.join()
+        # print(account)
+    print(f"Results: {results}")
 
 
 if __name__ == '__main__':
